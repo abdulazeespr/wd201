94f6c29506c92eaa4fdc318da9047732e933fb2e
abdulazeespr <abdulazeespr@gmail.com>
2024-09-04 13:12:51 -0700

    add eslint and prettier

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,28 +1,30 @@
+/* eslint-disable no-undef */
+
 const todoList = require("../todo");
 
-const {all,markAsComplete,add}= todoList();
-describe("Todolist Test Suite",()=>{
-    beforeAll(()=>{
-        add({
-            title:"Test tdoo",
-            dueDate:new Date().toISOString().split("T")[0],
-            completed:false,
-        });
-    })
-    test("should add new todo",()=>{
-      let todoitemcount = all.length;
-        add({
-            title:"Test tdoo",
-            dueDate:new Date().toISOString().split("T")[0],
-            completed:false,
-        })
-        expect(all.length).toBe(todoitemcount + 1);
-    })
+const { all, markAsComplete, add } = todoList();
 
-    test("should mark a todo as complete",()=>{
-        expect(all[0].completed).toBe(false);
-        markAsComplete(0);
-        expect(all[0].completed).toBe(true);
+describe("Todolist Test Suite", () => {
+  beforeAll(() => {
+    add({
+      title: "Test tdoo",
+      dueDate: new Date().toISOString().split("T")[0],
+      completed: false,
+    });
+  });
+  test("should add new todo", () => {
+    let todoitemcount = all.length;
+    add({
+      title: "Test tdoo",
+      dueDate: new Date().toISOString().split("T")[0],
+      completed: false,
+    });
+    expect(all.length).toBe(todoitemcount + 1);
+  });
 
-    })
-})
+  test("should mark a todo as complete", () => {
+    expect(all[0].completed).toBe(false);
+    markAsComplete(0);
+    expect(all[0].completed).toBe(true);
+  });
+});
