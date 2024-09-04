01606e5d4616022a880be84cfe6c3e445638730b
abdulazeespr <abdulazeespr@gmail.com>
2024-09-04 13:33:07 -0700

    add defination of all array

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -1,56 +1,64 @@
-
 const todoList = () => {
-    all = []
-    const add = (todoItem) => {
-      all.push(todoItem)
-    }
-    const markAsComplete = (index) => {
-      all[index].completed = true
-    }
-  
-    const overdue = () => {
-      // Write the date check condition here and return the array
-      // of overdue items accordingly.
-
-      return all.filter(item => item.dueDate < new Date().toISOString().split("T")[0]);
-    }
-  
-    const dueToday = () => {
-      // Write the date check condition here and return the array
-      // of todo items that are due today accordingly.
-      return all.filter(item => item.dueDate === new Date().toISOString().split("T")[0]);
-    }
-  
-    const dueLater = () => {
-      // Write the date check condition here and return the array
-      // of todo items that are due later accordingly.
-      return all.filter(item => item.dueDate > new Date().toISOString().split("T")[0]);
-    }
-  
-    const toDisplayableList = (list) => {
-      // Format the To-Do list here, and return the output string
-      // as per the format given above.
-        return list.map((item) =>{
-             //check if the todo is completed or not
-            const status = item.completed === true ? '[x]':'[ ]';
-            // set not date if it is dueDate is today
-            const dateORnot = item.dueDate === new Date().toISOString().split("T")[0] ? '':`${item.dueDate}`;
-
-            return `${status} ${item.title} ${dateORnot} `;
-        } 
-
-        ).join("\n");
-    }
-  
-    return {
-      all,
-      add,
-      markAsComplete,
-      overdue,
-      dueToday,
-      dueLater,
-      toDisplayableList
-    };
+  let all = [];
+  const add = (todoItem) => {
+    all.push(todoItem);
   };
-  
+  const markAsComplete = (index) => {
+    all[index].completed = true;
+  };
+
+  const overdue = () => {
+    // Write the date check condition here and return the array
+    // of overdue items accordingly.
+
+    return all.filter(
+      (item) => item.dueDate < new Date().toISOString().split("T")[0],
+    );
+  };
+
+  const dueToday = () => {
+    // Write the date check condition here and return the array
+    // of todo items that are due today accordingly.
+    return all.filter(
+      (item) => item.dueDate === new Date().toISOString().split("T")[0],
+    );
+  };
+
+  const dueLater = () => {
+    // Write the date check condition here and return the array
+    // of todo items that are due later accordingly.
+    return all.filter(
+      (item) => item.dueDate > new Date().toISOString().split("T")[0],
+    );
+  };
+
+  const toDisplayableList = (list) => {
+    // Format the To-Do list here, and return the output string
+    // as per the format given above.
+    return list
+      .map((item) => {
+        //check if the todo is completed or not
+        const status = item.completed === true ? "[x]" : "[ ]";
+        // set not date if it is dueDate is today
+        const dateORnot =
+          item.dueDate === new Date().toISOString().split("T")[0]
+            ? ""
+            : `${item.dueDate}`;
+
+        return `${status} ${item.title} ${dateORnot} `;
+      })
+      .join("\n");
+  };
+
+  return {
+    all,
+    add,
+    markAsComplete,
+    overdue,
+    dueToday,
+    dueLater,
+    toDisplayableList,
+  };
+};
+
 module.exports = todoList;
