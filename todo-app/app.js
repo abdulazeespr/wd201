d837f6eb7d5789a4a526a558072eda05891ac302
abdulazeespr <abdulazeespr@gmail.com>
2024-09-18 13:29:05 -0700

    add header and footer

--- todo-app/app.js
+++ todo-app/app.js
@@ -2,13 +2,21 @@ const express = require("express");
 const app = express();
 const { Todo } = require("./models");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 app.use(bodyParser.json());
 app.set("view engine", "ejs");
 
-app.get("/", function (request, response) {
-  response.render("index");
+app.get("/", async function (request, response) {
+  const allTodos = await Todo.getTodos();
+  if (request.accepts("html")) {
+    response.render("index", { allTodos });
+  }
+  response.json({ allTodos });
 });
+
+// eslint-disable-next-line no-undef
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/todos", async function (_request, response) {
   console.log("Processing list of all Todos ...");
