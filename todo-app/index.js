c8dca36e929029105b562113d5db442540ba4db8
abdulazeespr <abdulazeespr@gmail.com>
2024-09-18 12:17:12 -0700

    add todo test

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,53 +1,65 @@
-const express = require("express");
-const app = express();
-const { Todo } = require("./models");
-
-const bodyParser = require("body-parser");
-
-app.use(bodyParser.json());
-
-//get Todos
-app.get("/todos", async (request, response) => {
-  try {
-    const todo = await Todo.findAll();
-    return response.json(todo);
-  } catch (error) {
-    console.log(error);
-    return response.status(422).json(error);
-  }
-});
-
-//create Todo
-
-app.post("/todos", async (request, response) => {
-  try {
-    const todo = await Todo.addTodo({
-      title: request.body.title,
-      dueDate: request.body.dueDate,
-    });
-    return response.json(todo);
-  } catch (error) {
-    console.log(error);
-    return response.status(422).json(error);
-  }
-});
-//update Todo
-app.put("/todos/:id/markAsCompleted", async (request, response) => {
-  const todo = await Todo.findByPk(request.params.id);
-  console.log(request.params.id);
-  try {
-    const updateTodo = await todo.markAsCompleted();
-
-    return response.json(updateTodo);
-  } catch (error) {
-    return response.status(422).json(error);
-  }
-});
-//delete Todo
-app.delete("/todos/:id", (request, response) => {
-  response.json({ Todo: "todo is deleted" + request.params.id + "" });
-});
+const app = require("./app");
 
 app.listen(3000, () => {
-  console.log(`Server is start at 3000`);
+  console.log("Started express server at port 3000");
 });
+
+// const express = require("express");
+// const app = express();
+// const { Todo } = require("./models");
+
+// const bodyParser = require("body-parser");
+
+// app.use(bodyParser.json());
+
+// //get Todos
+// app.get("/todos", async (request, response) => {
+//   try {
+//     const todo = await Todo.findAll();
+//     return response.json(todo);
+//   } catch (error) {
+//     console.log(error);
+//     return response.status(422).json(error);
+//   }
+// });
+
+// //create Todo
+
+// app.post("/todos", async (request, response) => {
+//   try {
+//     const todo = await Todo.addTodo({
+//       title: request.body.title,
+//       dueDate: request.body.dueDate,
+//     });
+//     return response.json(todo);
+//   } catch (error) {
+//     console.log(error);
+//     return response.status(422).json(error);
+//   }
+// });
+// //update Todo
+// app.put("/todos/:id/markAsCompleted", async (request, response) => {
+//   const todo = await Todo.findByPk(request.params.id);
+
+//   try {
+//     const updateTodo = await todo.markAsCompleted();
+
+//     return response.json(updateTodo);
+//   } catch (error) {
+//     return response.status(422).json(error);
+//   }
+// });
+// //delete Todo
+// app.delete("/todos/:id", async (request, response) => {
+//   const todo = await Todo.findByPk(request.params.id);
+//   try {
+//     const deleteTodo = await todo.deleteTodo();
+//     response.json(deleteTodo);
+//   } catch (error) {
+//     return response.status(442).json(error);
+//   }
+// });
+
+// app.listen(3000, () => {
+//   console.log(`Server is start at 3000`);
+// });
--- todo-app/models/todo.js
+++ todo-app/models/todo.js
@@ -7,6 +7,7 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
+    // eslint-disable-next-line no-unused-vars
     static associate(models) {
       // define association here
     }
@@ -18,8 +19,12 @@ module.exports = (sequelize, DataTypes) => {
       });
     }
 
-    static markAsCompleted() {
+    markAsCompleted() {
       return this.update({ completed: true });
+    }
+
+    deleteTodo() {
+      return this.destroy();
     }
   }
   Todo.init(
