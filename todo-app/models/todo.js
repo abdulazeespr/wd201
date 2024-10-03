5bc8a2fb66ccaccba1b800802559a40f9b582873
abdulazeespr <abdulazeespr@gmail.com>
2024-10-03 15:22:40 -0700

    add validation

--- todo-app/models/todo.js
+++ todo-app/models/todo.js
@@ -115,9 +115,22 @@ module.exports = (sequelize, DataTypes) => {
   }
   Todo.init(
     {
-      title: DataTypes.STRING,
-      dueDate: DataTypes.DATEONLY,
-      completed: DataTypes.BOOLEAN,
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: true,
+          len: 5,
+        },
+      },
+      dueDate: {
+        type: DataTypes.DATEONLY,
+        allowNull: false,
+        validate: {
+          notNull: true,
+        },
+      },
+      completed: { type: DataTypes.BOOLEAN, defaultValue: false },
     },
     {
       sequelize,
